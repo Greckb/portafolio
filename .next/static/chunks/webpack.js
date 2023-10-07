01e24c3f4e890113f6931d692382a917a25b04f4
greckb <luar.28@gmail.com>
2023-10-07 14:03:31 +0200

    fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ca825367fb13a59d"; }
+/******/ 		__webpack_require__.h = function() { return "2891fa83581a2eb0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
